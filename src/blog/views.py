3b69f18ce447d5aecab4412c0b97da912fe7afe3
Emmanuel <degabriels03@gmail.com>
2023-08-05 16:00:26 +0530

    Added css and html to blog page Video 3

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Emmanuel',
+        'title': 'My First Blog',
+        'content': 'Content of my first blog',
+        'date_posted': 'August 8th 2023'
+    },
+    {
+        'author': 'Tony Star',
+        'title': 'Iron Man 4',
+        'content': 'Iron Man 4 is about get released coming month....',
+        'date_posted': 'August 10th 2023'
+    }
+]
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
