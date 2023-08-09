970dacf90034f6f6aa6614762e23aa6af3c6be71
Emmanuel <degabriels03@gmail.com>
2023-08-09 20:33:16 +0530

    Adding Posts and updating the database

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'Emmanuel',
-        'title': 'My First Blog',
-        'content': 'Content of my first blog',
-        'date_posted': 'August 8th 2023'
-    },
-    {
-        'author': 'Tony Star',
-        'title': 'Iron Man 4',
-        'content': 'Iron Man 4 is about get released coming month....',
-        'date_posted': 'August 10th 2023'
-    }
-]
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
