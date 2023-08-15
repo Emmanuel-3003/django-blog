16ca705137c75977348c3eb1fb56d545edb337af
Emmanuel <degabriels03@gmail.com>
2023-08-15 14:00:15 +0530

    Email and Password Reset

--- src/BlogProject/settings.py
+++ src/BlogProject/settings.py
@@ -131,3 +131,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "blog-home"
 LOGIN_URL = "login"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'degabriels03@gmail.com'#os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = 'ixnqktuqglhsndcv'#os.environ.get('EMAIL_PASS')
+DEFAULT_FORM_EMAIL = EMAIL_HOST_USER
